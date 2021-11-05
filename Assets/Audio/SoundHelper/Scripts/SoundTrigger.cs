15307b6a989a6f6133ee44eadec58d4cc98413ef
olarif <76481740+olarif@users.noreply.github.com>
2021-11-05 22:56:24 +0100

    more stuff and even some things

--- Assets/Audio/SoundHelper/Scripts/SoundTrigger.cs
+++ Assets/Audio/SoundHelper/Scripts/SoundTrigger.cs
@@ -2,18 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(BoxCollider))]
 public class SoundTrigger : MonoBehaviour
 {
+
     [SerializeField] private AudioSource soundSource;
     private bool hasPlayed = false;
     public bool loop = false;
 
+    public UnityEvent m_MyEvent;
+
     private BoxCollider _boxCollider;
+
+    void Start()
+    {
+        if (m_MyEvent == null)
+            m_MyEvent = new UnityEvent();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        m_MyEvent.Invoke();
+
         if (!other.CompareTag("Player")) return;
 
         if (!hasPlayed)
@@ -30,15 +42,11 @@ public class SoundTrigger : MonoBehaviour
                     hasPlayed = true;
                 }
             }
-                
-
         }
 
         //Don't play sound if it has already played.
         if (hasPlayed)
             return;
-
-        
     }
 
     private void OnDrawGizmosSelected()
